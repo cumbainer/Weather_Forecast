e3f8b40fc36f96767572af01c1438fdb5312b21c
cumbainer <shtaiie228666@gmail.com>
2022-12-23 20:17:36 +0200

    apikey is hidden now

--- src/main/java/com/weather/weather/service/Request.java
+++ src/main/java/com/weather/weather/service/Request.java
@@ -2,6 +2,10 @@ package com.weather.weather.service;
 
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -13,7 +17,7 @@ import java.net.http.HttpResponse;
 @Component
 //https://api.weatherbit.io/v2.0/forecast/daily?city=Kyiv&country=Ukraine&days=3&key=730be0b5fcab45aa9e9606ca38a6c281
 public class Request {
-    private final String API_KEY = "730be0b5fcab45aa9e9606ca38a6c281";
+    private final String API_KEY = getAPI_KEY();
 
     private final HttpClient httpClient = HttpClient.newHttpClient();
 
@@ -44,6 +48,19 @@ public class Request {
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
         return response.body();
 
+    }
+
+    @SneakyThrows
+    private String getAPI_KEY(){
+        File file = new File(
+                "D:\\java prog/APIKEY_WEATHER_2.txt");
+
+        BufferedReader br
+                = new BufferedReader(new FileReader(file));
+
+        String out = br.readLine();
+
+        return out;
     }
 
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into users(id, username, password, role) values (15, 'vlad', '123', 'ADMIN');
+insert into users(id, username, password, role) values (14, 'vadim', '321', 'USER');
