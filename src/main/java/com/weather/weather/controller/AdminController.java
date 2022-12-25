b3f2d6ebfc33abf908440be000463e9c7a2c014b
cumbainer <shtaiie228666@gmail.com>
2022-12-25 10:42:34 +0200

    now can remove users

--- src/main/java/com/weather/weather/controller/AdminController.java
+++ src/main/java/com/weather/weather/controller/AdminController.java
@@ -3,12 +3,15 @@ package com.weather.weather.controller;
 
 import com.weather.weather.entity.User;
 import com.weather.weather.repo.UserRepo;
+import com.weather.weather.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,13 +19,21 @@ import java.util.List;
 @RequestMapping("/admin")
 @AllArgsConstructor
 public class AdminController {
-    private UserRepo userRepo;
+    private UserService userService;
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/users")
     public String getUsers(Model model){
-        List<User> users = userRepo.findAll();
+        List<User> users = userService.getAll();
         model.addAttribute("users", users);
         return "users";
+    }
+
+
+    @PreAuthorize("hasRole('ROLE_ADMIN') or authentication.principal.id == #id")
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable("id") long id) {
+        userService.delete(id);
+        return "redirect:/admin/users";
     }
 }
--- src/main/java/com/weather/weather/service/UserService.java
+++ src/main/java/com/weather/weather/service/UserService.java
@@ -4,11 +4,17 @@ import com.weather.weather.entity.User;
 import com.weather.weather.exception.NullEntityReferenceException;
 import com.weather.weather.repo.UserRepo;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 @AllArgsConstructor
 @Service
+@Slf4j
 public class UserService {
 
     private final UserRepo userRepo;
@@ -21,5 +27,31 @@ public class UserService {
             return userRepo.save(user);
         }
         throw new NullEntityReferenceException("User cannot be 'null'");
+    }
+
+
+
+    public User readById(long id) {
+        log.info("READ BY ID iS CALLED: " + userRepo.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("User with id " + id + " not found")));
+        return userRepo.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("User with id " + id + " not found"));
+
+    }
+
+
+
+
+
+    public void delete(long id) {
+        userRepo.delete(readById(id));
+        log.info("USER DELETED WITH ID: " + id);
+    }
+
+
+    public List<User> getAll() {
+        List<User> users = userRepo.findAll();
+        log.info("GET ALL IS CALLED: "  + userRepo.findAll());
+        return users.isEmpty() ? new ArrayList<>() : users;
     }
 }
