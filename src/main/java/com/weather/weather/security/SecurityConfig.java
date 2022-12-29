38cd04dd19f43663cd16c880697406f0b4fd3697
cumbainer <shtaiie228666@gmail.com>
2022-12-29 22:27:58 +0200

    added password encoder

--- src/main/java/com/weather/weather/security/SecurityConfig.java
+++ src/main/java/com/weather/weather/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -53,9 +54,10 @@ public class SecurityConfig {
 
     }
 
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(10);
     }
 
 
